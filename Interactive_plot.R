dd4ef59eb51d5d3d91da0e10569f1432b2f600ea
Annie Xu <xxu0116@uw.edu>
2024-05-09 17:40:23 -0700

    Render in Markdown?

--- Interactive_plot.R
+++
@@ -1,4 +0,0 @@
-# Let me try to put the interactive plot in here
-
-<iframe src="https://github.com/anniexu0116/Mapping_trial/blob/main/Visualization/2018_map_interactive.html" width="600" height="400"></iframe>
-  
